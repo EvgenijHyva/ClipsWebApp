2c82695cd9d5aabbb96829937c86f5171cb8f95c
Evgeny <evgeny.hyvarinen@gmail.com>
2022-11-10 21:20:45 +0200

    Added interaction to user-auth modal

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
+@Injectable({ // injected in global scope
   providedIn: 'root'
 })
 export class ModalService {
-  visible: boolean = false;
   
+  private visible: boolean = false;
+
+  isModalVisible(): boolean {
+    return this.visible;
+  }
+  toggleModal():void {
+    this.visible = !this.visible;
+  }
 
   constructor() { }
 }
