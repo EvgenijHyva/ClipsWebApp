977334f5f6d78921a679bdb8c3bf18b1f499e51e
Evgeny <evgeny.hyvarinen@gmail.com>
2022-11-12 23:34:08 +0200

    Register form refacoring, added validation into inputs, cleanups, register behaviour preparations

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
+	name = new FormControl('', [
+		Validators.required,
+		Validators.minLength(3)
+	])
+	email = new FormControl('', [
+		Validators.required,
+		Validators.email
+	])
+	age = new FormControl('', [
+		Validators.max(120),
+		Validators.min(14)
+	])
+	password = new FormControl('', [
+		Validators.required,
+		Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+	])
+	confirm_password = new FormControl('', [
+		Validators.required,
+	])
+	phone_number = new FormControl('', [
+		Validators.required,
+		Validators.maxLength(21),
+		Validators.minLength(21)
+	])
 
-  constructor() { }
+	registerForm: FormGroup = new FormGroup({
+		name: this.name,
+		email: this.email,
+		age: this.age,
+		password: this.password,
+		confirm_password: this.confirm_password,
+		phone_number: this.phone_number
+	});
 
-  ngOnInit(): void {
-  }
-
+	register() {
+		console.log("this")
+	}
 }
