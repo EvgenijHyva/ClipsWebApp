ecca6aa4ea5cf1d244f7f8772628fac812735375
Evgeny <evgeny.hyvarinen@gmail.com>
2022-11-14 18:30:56 +0200

    login form functionality based on template forms

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  
+  creadentials: { email: string, password: string } = {
+    email: '',
+    password: ''
+  }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    console.log("login ",this.creadentials)
   }
 
 }
