d58496fc02ea8486b81cdd7c18c6822e18e8cba2
Evgeny <evgeny.hyvarinen@gmail.com>
2022-11-21 19:50:13 +0200

    working on Clip component, added route and parameters to router

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-  selector: 'app-clip',
-  templateUrl: './clip.component.html',
-  styleUrls: ['./clip.component.css']
+	selector: 'app-clip',
+	templateUrl: './clip.component.html',
+	styleUrls: ['./clip.component.css']
 })
 export class ClipComponent implements OnInit {
+	id: string = ''
 
-  constructor() { }
+	constructor(
+		public readonly route: ActivatedRoute
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.route.params.subscribe((params:Params) => {
+			this.id = params.id
+		})
+	}
 
 }
