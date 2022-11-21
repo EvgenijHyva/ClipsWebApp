c97a69e99c211e04972e006e5d9b518690d92462
Evgeny <evgeny.hyvarinen@gmail.com>
2022-11-21 20:01:36 +0200

    Updaiong route parameters

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
 	selector: 'app-manage',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
+	videoOrder: string = '1'
 
-	constructor() { }
+	constructor(
+		private readonly router: Router,
+		private readonly route: ActivatedRoute
+	) { }
+
+	sort(event: Event) {
+		const { value } = (event.target as HTMLSelectElement)
+
+		this.router.navigateByUrl(`/manage?sort=${value}`)
+	}
 
 	ngOnInit(): void {
+		this.route.queryParamMap.subscribe((params: Params) => {
+			this.videoOrder = params.sort === '2' ? params.sort: '1'
+		})
 	}
 
 }
