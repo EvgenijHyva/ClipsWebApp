97da098858f1950be229dbbefafaf09a3cefa106
Evgeny <evgeny.hyvarinen@gmail.com>
2022-11-22 21:13:46 +0200

    Added uuid-package + types, added alert component into template. Working on upload component: added percentage with observable, uploading into firebase, reactive form module for uploading form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
+import { environment } from 'src/environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
-import { AngularFireModule } from "@angular/fire/compat";
-import { environment } from 'src/environments/environment';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -31,6 +31,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     AngularFireModule.initializeApp(environment.firebase), // init with credentials in env
     AngularFireAuthModule,
     AngularFirestoreModule, // communicating with firebase 
+    AngularFireStorageModule,
     VideoModule,
     AppRoutingModule
   ],
--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
+import { AlertColorEnum } from 'src/app/shared/alert/alert.component';
+
 
 @Component({
     selector: 'app-upload',
@@ -10,16 +15,67 @@ export class UploadComponent implements OnInit {
     isDragover: boolean = false;
     file: File | null = null;
     nextStep: boolean = false;
+    alertMessage!: string;
+    showAlertMessage: boolean = false;
+    alertMessageColor: AlertColorEnum = AlertColorEnum.BLUE;
+    inSubmission: boolean = false;
+    precentage: number = 0;
 
-    constructor() { }
+    constructor(
+        private readonly storage: AngularFireStorage
+    ) { }
+
+    title = new FormControl('', { 
+        validators: [
+            Validators.required,
+            Validators.minLength(3)
+        ], 
+        nonNullable:true
+    })
+
+    // thumbnail = new FormControl(null, [
+    //     Validators.required,
+    // ])
+
+    form: FormGroup = new FormGroup({
+        title: this.title,
+        // thumbnail: this.thumbnail
+    })
+
+    uploadFile(): void {
+        this.inSubmission = true;
+        this.alertMessageColor = AlertColorEnum.BLUE;
+        this.alertMessage = "Please wait! Your clip i being uploaded";
+        this.showAlertMessage = true;
+
+        const clipFileName = `${this.title.value}_${uuid()}`
+        const clipPath = `clips/${clipFileName}.mp4`;
+        // observable for upload progress
+        const task = this.storage.upload(clipPath, this.file)
+        task.percentageChanges().subscribe(progress => {
+            this.precentage = progress as number / 100;
+        }) 
+    }
 
     storeFile(event: Event): void {
         this.isDragover = false
         this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null
-        if (!this.file || this.file.type !== 'video/mp4') {
-            return
-        }
+        const { name } = this.file!
+        // if (!this.file || this.file.type !== 'video/mp4') {
+        //     return
+        // }
+        this.title.setValue(name.replace(/\.[^/.]+$/, ''))
         this.nextStep = true;
+    }
+
+    resetToDefault() {
+        this.inSubmission = false;
+        this.alertMessage;
+        this.alertMessageColor = AlertColorEnum.BLUE;
+        this.showAlertMessage = false;
+        this.file = null;
+        this.title.setValue('');
+        this.nextStep = false;
     }
 
     ngOnInit(): void {
