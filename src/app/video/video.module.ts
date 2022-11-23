10b56f7f64579f944d3e50e8ebaa5a737215719a
Evgeny <evgeny.hyvarinen@gmail.com>
2022-11-23 19:39:44 +0200

    Created video editing component

--- src/app/video/video.module.ts
+++ src/app/video/video.module.ts
@@ -6,12 +6,14 @@ import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EditComponentComponent } from './edit-component/edit-component.component';
 
 
 @NgModule({
   declarations: [
     ManageComponent,
-    UploadComponent
+    UploadComponent,
+    EditComponentComponent
   ],
   imports: [
     CommonModule,
