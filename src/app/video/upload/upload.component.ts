b84187e908b55bbdcf2dc814d3bdf8e90d36a7b0
Evgeny <evgeny.hyvarinen@gmail.com>
2022-11-23 13:59:50 +0200

    added redirect to clip page after uploading

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,6 +7,7 @@ import { last, switchMap } from 'rxjs/operators';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
 import { ClipService } from 'src/app/services/clip.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-upload',
@@ -29,7 +30,8 @@ export class UploadComponent implements OnDestroy {
     constructor(
         private readonly storage: AngularFireStorage,
         private readonly auth: AngularFireAuth,
-        private readonly clipsService: ClipService
+        private readonly clipsService: ClipService,
+        private readonly router: Router
     ) { 
         auth.user.subscribe(user => this.user = user)
     }
@@ -73,7 +75,7 @@ export class UploadComponent implements OnDestroy {
             last(),
             switchMap(() => clipRef.getDownloadURL()) // inner observable will replace snapshot by reference
         ).subscribe({
-            next: (url) => { 
+            next: async (url) => { 
                 const clip = { 
                     uid: this.user!.uid as string,
                     displayName: this.user!.displayName as string,
@@ -82,14 +84,17 @@ export class UploadComponent implements OnDestroy {
                     url
                 }
                 
-                this.clipsService.createClip(clip)
+                const clipDocumentRef = await this.clipsService.createClip(clip)
 
                 this.alertMessageColor = AlertColorEnum.GREEN;
                 this.alertMessage = 'Success, your clip is uploaded.';
                 this.showPercentage = false;
-                setTimeout(() => {
-                    this.resetToDefault()
-                }, 4000)
+
+                setTimeout((): void => {
+                    this.router.navigate([
+                        'clip', clipDocumentRef.id
+                    ])
+                }, 1500)
             },
             error: (error) => {
                 if (error.code == "storage/unknown" ) console.log("Upload canceled")
@@ -118,6 +123,7 @@ export class UploadComponent implements OnDestroy {
     }
 
     resetToDefault() {
+        // for future use
         this.inSubmission = false;
         this.alertMessage;
         this.alertMessageColor = AlertColorEnum.BLUE;
