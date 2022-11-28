06d539fba986178b81ff2bdc62223b06c0f90002
Evgeny <evgeny.hyvarinen@gmail.com>
2022-11-28 17:12:01 +0200

    Ffmpeg service initialization

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { createFFmpeg, FFmpeg } from '@ffmpeg/ffmpeg';
+
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class FfmpegService {
+	isReady: boolean = false;
+	private ffmpeg: FFmpeg;
 
-  constructor() { }
+	constructor() { 
+		this.ffmpeg = createFFmpeg({ log: true }) // debugging commands
+	}
+
+	async init(): Promise<void> {
+		if (this.isReady) {
+			return;
+		}
+
+		await this.ffmpeg.load();
+		this.isReady = true;
+	}
 }
