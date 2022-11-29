dd170f355ef05192ff7708f71c6d7c8308a895c7
Evgeny <evgeny.hyvarinen@gmail.com>
2022-11-29 15:46:21 +0200

    created function for extracting screenshots from random video duration. Screenshots are blob format

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -7,6 +7,7 @@ import { createFFmpeg, FFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 })
 export class FfmpegService {
 	isReady: boolean = false;
+	currentVideoDuration: number = 0;
 	private ffmpeg: FFmpeg;
 
 	constructor() { 
@@ -25,22 +26,52 @@ export class FfmpegService {
 	async getScreenshots(file: File) {
 		const data = await fetchFile(file); // convert to binary data
 		this.ffmpeg.FS('writeFile', file.name, data); //File system: read and write files
-		// series of screenshots
-		const seconds = [1,2,3];
-		const commands: string[] = [];
-		// documentation for ffmpeg use:
-		// https://www.ffmpeg.org/
-		for (let second of seconds) {
-			commands.push(
-				'-i', file.name, // input feed
-				'-ss', `00:00:0${second}`, // output options: screenshot position 'hh:mm:ss' 
-				'-frames:v', '1', // screenshots counter (single frame)
-				'-filter:v', 'scale=510:-1', // output size 'width:height' -> aspect ratio (-1) will be calculated by ffmpeg
-				`output_0${second}.png` // output file name and type
-			)
+		// series of random screenshots
+		const videoDuration = await getVideoDuration(file);
+		const timeArray: string[] = [];
+		for(let i:number = 0; i < 3; i++) {
+			let secondSnapshot = Math.floor(Math.random() * videoDuration)
+			let time = new Date(secondSnapshot * 1000).toUTCString().match(/(\d\d:\d\d:\d\d)/)![0];
+			timeArray.push(time)
 		}
 
+		const commands: string[] = [];
+
+		// documentation for ffmpeg use:
+		// https://www.ffmpeg.org/
+		for (let time of timeArray) {
+			commands.push(
+				'-i', file.name, // input feed
+				'-ss', time, // output options: screenshot position 'hh:mm:ss' 
+				'-frames:v', '1', // screenshots counter (single frame)
+				'-filter:v', 'scale=510:-1', // output size 'width:height' -> aspect ratio (-1) will be calculated by ffmpeg
+				`output_${time}.png` // output file name and type
+			)
+		}
 		await this.ffmpeg.run(...commands)
-		return;
+
+		const screenshots: string[] = [];
+		timeArray.forEach(time => {
+			const screenshotFile = this.ffmpeg.FS('readFile', `output_${time}.png`)
+			const screenshotBlob = new Blob(
+				[screenshotFile.buffer], {
+					type: 'image/png'
+				}
+			);
+			const screenshotURL = URL.createObjectURL(screenshotBlob)
+			screenshots.push(screenshotURL)
+		})
+		return screenshots
 	}
 }
+export const getVideoDuration = (file:File):Promise<number> =>
+	new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.onload = () => {
+			const media = new Audio(reader.result as string);
+			media.onloadedmetadata = () => resolve(media.duration);
+		};
+		reader.readAsDataURL(file);
+		reader.onerror = error => reject(error);
+	});
+
