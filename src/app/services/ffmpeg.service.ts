fab0d080aa885eff5f065b1fc0a0ababea82eaa7
Evgeny <evgeny.hyvarinen@gmail.com>
2022-11-29 13:56:53 +0200

    get screenshots by using ffmpeg

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -25,6 +25,22 @@ export class FfmpegService {
 	async getScreenshots(file: File) {
 		const data = await fetchFile(file); // convert to binary data
 		this.ffmpeg.FS('writeFile', file.name, data); //File system: read and write files
+		// series of screenshots
+		const seconds = [1,2,3];
+		const commands: string[] = [];
+		// documentation for ffmpeg use:
+		// https://www.ffmpeg.org/
+		for (let second of seconds) {
+			commands.push(
+				'-i', file.name, // input feed
+				'-ss', `00:00:0${second}`, // output options: screenshot position 'hh:mm:ss' 
+				'-frames:v', '1', // screenshots counter (single frame)
+				'-filter:v', 'scale=510:-1', // output size 'width:height' -> aspect ratio (-1) will be calculated by ffmpeg
+				`output_0${second}.png` // output file name and type
+			)
+		}
+
+		await this.ffmpeg.run(...commands)
 		return;
 	}
 }
