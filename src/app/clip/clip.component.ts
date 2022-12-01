f88386547b426be8c4d8ecfe971bea9ed3e3d741
Evgeny <evgeny.hyvarinen@gmail.com>
2022-12-01 19:29:19 +0200

    Global styles transfered into clip-component styles, disabled default css-encapsulation behaviour from clip-component, changed video component behaviour to videojs.player

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import videojs from 'video.js';
 
 @Component({
 	selector: 'app-clip',
 	templateUrl: './clip.component.html',
-	styleUrls: ['./clip.component.css']
+	styleUrls: ['./clip.component.css'],
+	encapsulation: ViewEncapsulation.None
 })
 export class ClipComponent implements OnInit {
-	id: string = ''
+	id: string = '';
+	@ViewChild('videoPlayer', { static: true }) target?: ElementRef; // static: true, decorator will update property before ngOnInit
+	player?: videojs.Player;
 
 	constructor(
 		public readonly route: ActivatedRoute
 	) { }
 
 	ngOnInit(): void {
+		this.player = videojs(this.target?.nativeElement);
+
 		this.route.params.subscribe((params:Params) => {
 			this.id = params.id
 		})
