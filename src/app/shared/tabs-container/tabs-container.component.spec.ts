51dbdb104bfe57156c340914020add6689aabe37
Evgeny <evgeny.hyvarinen@gmail.com>
2022-12-03 15:44:43 +0200

    Created dummy component for content projection. Testing tab-container for tabs property. (2 ways for testing)

--- src/app/shared/tabs-container/tabs-container.component.spec.ts
+++ src/app/shared/tabs-container/tabs-container.component.spec.ts
@@ -1,23 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TabsContainerComponent } from './tabs-container.component';
+// Tabs-conteiner component uses content projection for inserting content
+// For test need to create dummy host component
+import { Component } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+import { By } from '@angular/platform-browser';
+
+@Component({
+    template: `<app-tabs-container>
+		<app-tab tabTitle="Tab1">Tab 1</app-tab>
+		<app-tab tabTitle="Tab2">Tab 2</app-tab>
+	</app-tabs-container>`
+}) 
+class DummyHostComponent {}
+
 
 describe('TabsContainerComponent', () => {
-  let component: TabsContainerComponent;
-  let fixture: ComponentFixture<TabsContainerComponent>;
+	let component: DummyHostComponent;
+	let fixture: ComponentFixture<DummyHostComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TabsContainerComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+		declarations: [ TabsContainerComponent, TabComponent, DummyHostComponent ]
+		})
+		.compileComponents();
 
-    fixture = TestBed.createComponent(TabsContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(DummyHostComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('Should create dummy component for loading nested components with projected content', () => {
+		expect(component).toBeTruthy();
+	});
+
+	// test content projection of component
+	// component have a property tabs decorated @ContentChildren on projected content
+	// template render list item elements
+	it('Should have 2 tabs', () => {
+		const tabs = fixture.debugElement.queryAll(By.css('li'));
+		expect(tabs.length).toBe(2);
+	});
+
+	it('Should have 2 tabs, alternative test.', () => {
+		// different source of tabs values. 
+		const containerComponent = fixture.debugElement.query(
+			By.directive(TabsContainerComponent
+		));
+		const tabsProp = containerComponent.componentInstance.tabs;
+		expect(tabsProp.length).toBe(2);
+	})
 });
