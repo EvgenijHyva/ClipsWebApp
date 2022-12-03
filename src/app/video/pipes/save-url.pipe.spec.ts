8b248e2dc4411959db794fc8f838704cea855286
Evgeny <evgeny.hyvarinen@gmail.com>
2022-12-03 18:08:56 +0200

    deleted spec files

--- src/app/video/pipes/save-url.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SaveURLPipe } from './save-url.pipe';
-
-describe('SaveURLPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SaveURLPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
