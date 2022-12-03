5b8c54ae70e3edef16235e5e3e5978b770cd7323
Evgeny <evgeny.hyvarinen@gmail.com>
2022-12-03 16:43:51 +0200

    .gitignore for broken spec. Created mockedAuthService for user auth observable. Injected Dependency in nav tests

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavComponent } from './nav.component';
+import { of } from 'rxjs';
+
+// component depends on auth.observable. 
+// Unit tests are isolated.
+// Dependencies have to be mocked
+import { AuthService } from '../services/auth.service';
 
 describe('NavComponent', () => {
-  let component: NavComponent;
-  let fixture: ComponentFixture<NavComponent>;
+	let component: NavComponent;
+	let fixture: ComponentFixture<NavComponent>;
+	// will create an object insted of reating a regular object. Jasmin will spy on methods of object
+	const mockedAuthService = jasmine.createSpyObj(
+		'AuthService', 
+		['createUser', 'logout'], 
+		{
+			isAuthenticated$: of(true)
+		}
+	); // creating fake dependency 
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ NavComponent ],
+			// Injection mocked Dependency
+			providers: [ 
+				{ 
+					provide: AuthService,
+					useValue: mockedAuthService
+				} 
+			]
+		})
+		.compileComponents();
 
-    fixture = TestBed.createComponent(NavComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(NavComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
